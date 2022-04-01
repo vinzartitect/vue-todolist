59b53b5dd029cb63fa1dd7eb3dec40fd03a638ab
VincenzoBorriello89 <vincborr89@gmail.com>
2022-04-01 18:34:41 +0200

    esercizio ultimato

--- asset/js/main.js
+++ asset/js/main.js
@@ -1,18 +1,76 @@
 // Descrizione:
-// Stampare a schermo un messaggio all’interno di un h1, utilizzando i data.
+// Rifare l'esercizio della to do list.
+// Ogni todo sarà un oggetto, formato da due proprietà:
+// - text, una stringa che indica il testo del todo
+// - done, un booleano (true/false) che indica se il todo è stato fatto oppure no
+// MILESTONE 1
+// Stampare all'interno di una lista, un item per ogni todo.
+// Se la proprietà done è uguale a true, visualizzare il testo del todo sbarrato.
+// MILESTONE 2
+// Visualizzare a fianco ad ogni item ha una "x": cliccando su di essa, 
+// il todo viene rimosso dalla lista.
+// MILESTONE 3
+// Predisporre un campo di input testuale e un pulsante "aggiungi": cliccando 
+// sul pulsante, il testo digitato viene letto e utilizzato per creare un nuovo todo, 
+// che quindi viene aggiunto alla lista dei todo esistenti.
 // Bonus:
-// Aggiungere alla pagina un’immagine, presa anch’essa da un data.
+// 1- oltre al click sul pulsante, intercettare anche il tasto ENTER per aggiungere 
+// il todo alla lista
+// 2- cliccando sul testo dell'item, invertire il valore della proprietà done 
+// del todo corrispondente (se done era uguale a false, impostare true e viceversa)
+// BONUS extra:
+// non permettere l'inserimento di todo doppie nell'array di oggetti
 
-// creiamo la variabile app che contiene il collegamento al div
-// e l'insieme degli oggetti al suo interno
 var app = new Vue (
     {
         el: '#root',
 
         data: {
-            content: 'Piloti F1 2022',
-            classi: ['color_white', 'font_big'],
-            photo: './asset/img/F1_2022_Line_Up.jpg',
+            newTodo: '',
+            todos: [
+                {
+                    text:'Fare i compiti',
+                    done: false
+                },
+                {
+                    text:'Fare la spesa',
+                    done: false
+                },
+                {
+                    text:'Fare il bucato',
+                    done: false
+                },
+            ],
+            
+        },
+
+        methods: {
+
+            removeTodo: function(todoIndex){
+                this.todos.splice(todoIndex,1);
+            },
+
+            addTodo: function(){
+                let objTodo = {
+                    text: this.newTodo,
+                    done: false,
+                }
+
+                if (!this.newTodo == ''){
+
+                    this.todos.push(objTodo);
+                    this.newTodo = '';
+                }
+            },
+
+            done: function(element){
+                if(element.done == true){
+                    element.done = false
+                } else {
+                    element.done = true
+                }
+            }
+
         }
 
     }
